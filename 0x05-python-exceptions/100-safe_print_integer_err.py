29c26a8af814773516e4f72df4a781c3450c18f8
Oyetola israel <oyetolaisreal01@gmail.com>
2024-01-24 19:16:16 -0800

    correction

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,7 +4,7 @@ import sys
 
 
 def safe_print_integer_err(value):
-    """Prints an integer with "{:d}.format().
+    """Prints an integer with "{:d}".format().
 
     If a ValueError message is caught, a corresponding
     message is printed to standard error.
@@ -17,7 +17,7 @@ def safe_print_integer_err(value):
         Otherwise - True.
     """
     try:
-        print("{:d}.format(value))
+        print("{:d}".format(value))
         return (True)
     except (TypeError, ValueError):
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
